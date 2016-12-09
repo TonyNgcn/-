c088c4ee81276fb34252cadc411462a678b522e8
TonyNgcn <649044528@qq.com>
2016-12-09 10:24:23 +0800

    多项式加法

---
+++ ConsoleApplication1/1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 101
+int main(void)
+{
+	int count = 0, plus = 0;
+	int a[N] = { 0 }, b[N] = { 0 }, temp, n;
+	while (scanf("%d%d", &n, &temp))
+	{
+		a[n] = n;
+
+	}
+
+	system("pause");
+	return 0;
+}
