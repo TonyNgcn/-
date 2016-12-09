c42683b9abf1840b409dd313dcefc6dc41f33f22
TonyNgcn <649044528@qq.com>
2016-12-09 10:40:39 +0800

    多项式加法

--- ConsoleApplication1/1.c
+++ ConsoleApplication1/1.c
@@ -7,10 +7,21 @@ int main(void)
 	int a[N] = { 0 }, b[N] = { 0 }, temp, n;
 	while (scanf("%d%d", &n, &temp))
 	{
-		a[n] = n;
-
+		a[n] += temp;
+		b[n] = n;
+		if (n == 0)
+			count++;
+		if (count == 2)
+			break;
 	}
+	for (int i = 100; i > 0; i--)
+	{
+		if (plus != 0)
+		{
+			printf("+");
 
+		}
+	}
 	system("pause");
 	return 0;
 }
